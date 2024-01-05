f3baec0504aaa8cff5d81d4e7a0d5c25bbe47941
okadirkartal <161307071@kocaeli.edu.tr>
2024-01-05 22:47:40 +0400

    refactor

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -14,59 +14,61 @@ const connectDB = async () => {
                 useUnifiedTopology: true
             }
         )
-        .then(() => console.log('DB Connected'))
-        .catch(() => `An error occured while connecting to DB`);
+        .then(() =>  {
+            console.log('DB Connected');
+            seed(); 
+        })
+        .catch(() => `An error occured while connecting to DB`);  
+}
 
+function createUsers(userIds) {
 
-    const userIds = ["65977a1f006a450701d43d6f","65977a1f006a450701d43d70","65977a1f006a450701d43d71"];
-       
-    function createUsers() {
+    for(let i = 0; i < userIds.length; i++) {
+        const user = new User({
+            _id: new ObjectId(userIds[i]),
+            userName: `user_${i}`,
+            email: `test_${i}@gmail.com`
+       });
+    user.save();
+    }  
+}
+
+function createAccounts(userIds) {
+    const accountIds = ["65977d1867fe0da87be0f868","65977d1867fe0da87be0f869","65977d1867fe0da87be0f86b"];
 
         for(let i = 0; i < userIds.length; i++) {
-            const user = new User({
-                _id: new ObjectId(userIds[i]),
-                userName: `user_${i}`,
-                email: `test_${i}@gmail.com`
-           });
-        user.save();
-        }  
-    }
-
-    function createAccounts() {
-        const accountIds = ["65977d1867fe0da87be0f868","65977d1867fe0da87be0f869","65977d1867fe0da87be0f86b"];
-
-            for(let i = 0; i < userIds.length; i++) {
-                const account = new Account({
-                    _id: new ObjectId(accountIds[i]),
-                    userId: userIds[i],
-                    balance: 1000
-                });
-                account.save();
-            }
-    }
-
-    const seed =  () => {
-        try {
-
-            User.countDocuments().then((count) => {
-                if (count === 0) {
-                    createUsers();
-                }
+            const account = new Account({
+                _id: new ObjectId(accountIds[i]),
+                userId: userIds[i],
+                balance: 1000
             });
-
-            Account.countDocuments().then((count) => {
-                if (count === 0) {
-                    createAccounts();
-                }
-            });
-
-            console.log("Database seeded 🌱");
-        } catch (x) {
-            console.error(x);
+            account.save();
         }
-    };
-
-    seed();
 }
+
+const seed =  () => {
+
+    const userIds = ["65977a1f006a450701d43d6f","65977a1f006a450701d43d70","65977a1f006a450701d43d71"];
+   
+    try {
+
+        User.countDocuments().then((count) => {
+            if (count === 0) {
+                createUsers(userIds);
+            }
+        });
+
+        Account.countDocuments().then((count) => {
+            if (count === 0) {
+                createAccounts(userIds);
+            }
+        });
+
+        console.log("Database seeded 🌱");
+    } catch (x) {
+        console.error(x);
+    }
+};
+
 
 module.exports = connectDB;
